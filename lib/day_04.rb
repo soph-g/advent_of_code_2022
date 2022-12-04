4a3ab2287854eed10860c8bb91347d0f07be23a6
Sophie Gill <sophie-gill@hotmail.com>
2022-12-04 20:05:49 +0000

    🧼 - Day 4 Part 2

--- lib/day_04.rb
+++ lib/day_04.rb
@@ -1,6 +1,6 @@
 require_relative './utils'
 
-test = true
+test = false
 day = '04'
 arr = create_array(file_path(day, test))
 
@@ -19,6 +19,27 @@ arr.each do |pairs|
 
   if first[1].to_i >= second[1].to_i
     result << pairs
+    count += 1
+  end
+end
+
+p count
+
+# Part 2
+
+count = 0
+
+arr.each do |pairs|
+  x = pairs[0].map { |s| s.to_i }
+  y = pairs[1].map { |s| s.to_i }
+
+  if x[0] >= y[0] && x[0] <= y[1]
+    count += 1
+  elsif x[1] >= y[0] && x[1] <= y[1]
+    count += 1
+  elsif y[0] >= x[0] && y[0] <= x[1]
+    count += 1
+  elsif y[1] >= x[0] && y[1] <= x[1]
     count += 1
   end
 end
